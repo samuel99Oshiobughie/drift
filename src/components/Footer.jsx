3b49b0b7aefbfcaf041d2335a52dcbbafda24a60
Samuel oshiobughie <Cedar@MACs-MacBook-Pro.local>
2022-10-02 15:16:36 +0100

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => (
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
+      <p className="text-white text-sm text-center">Come join us and hear for the wondeful experience</p>
       <p className="text-white text-sm text-center font-medium mt-2">info@Drift.com</p>
     </div>
 
